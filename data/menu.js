12dec74e43749975a4f41f33d5819a83a79be46e
Harrison Chachko <hchachko@gmail.com>
2022-04-21 13:49:44 -0400

    Added menu item page and review page
    Quick notes:
    -Added a menu item page, which displays details about each item from the menu collection
    -Added a review page, which contains a form for users to post reviews to the review data collection
    -Reorganized handlebar menu pages
    -Other small changes etc.

--- data/menu.js
+++ data/menu.js
@@ -1,5 +1,6 @@
 const mongoCollections = require("../config/mongoCollections");
 const menu = mongoCollections.menu;
+let { ObjectId } = require('mongodb');
 
 module.exports = {
     getAll: async ()=>{
@@ -10,16 +11,16 @@ module.exports = {
         }
         return menuList;
     },
-    async get(id){  //Still a work in progress (Harrison)
+    async get(id){
         if (arguments.length != 1) throw "Usuage: get(id)";
         if (typeof id != 'string') throw "Non-string input detected";
         id = id.trim();
         if (id.length == 0) throw "Empty string input detected";
         if (!ObjectId.isValid(id)) throw "id is not a valid Object ID";
         const menuCollection = await menu();
-        const menu = await menuCollection.findOne({ _id: ObjectId(id) });
-        if (menu === null) throw 'No item with that id';
-        menu['_id'] = menu['_id'].toString();
-        return menu;
+        const menuItem = await menuCollection.findOne({ _id: ObjectId(id) });
+        if (menuItem === null) throw 'No item with that id';
+        menuItem['_id'] = menuItem['_id'].toString();
+        return menuItem;
     }
 }
--- routes/menu.js
+++ routes/menu.js
@@ -7,33 +7,43 @@ router.get("/", async (req, res) => {
     if (req.session.user) {
       try{
         const menu = await menuData.getAll();
-        res.render("site/menu", {firstName: req.session.user.firstName, menu: menu});
+        res.render("site/menuPages/menu", {firstName: req.session.user.firstName, menu: menu});
       } catch(e) {
-        res.status(404).json({ message: e }); //TODO error out to a html page instead
+        res.status(400).render("site/homepage", { error: e });
       }
     } else {
       res.redirect("/");
     }
   });
-  //.get(id) is still a work in progress (Harrison)
-  router.get("/:id", async (req, res) => {
-    if (req.session.user) {
-      try{
-        if (!req.params.id) throw "id needed";
-        if (!ObjectId.isValid(req.params.id)) throw "id is not a valid Object ID";
-        const menuItem = await menuData.get(req.params.id);
-        res.render("site/menuItem", {menuItem: menuItem[0]});
-      } catch(e) {
-        res.status(404).json({ message: e }); //TODO error out to a html page instead
-      }
-    } else {
-      res.redirect("/");
+
+router.get("/:id", async (req, res) => {
+  if (req.session.user) {
+    try{
+      if (!req.params.id) throw "id needed";
+      if (!ObjectId.isValid(req.params.id)) throw "id is not a valid Object ID";
+      const menuItem = await menuData.get(req.params.id);
+      res.render("site/menuPages/menuItem", {menuItem: menuItem});
+    } catch(e) {
+      res.status(400).render("site/homepage", { error: e });
     }
-  });
+  } else {
+    res.redirect("/");
+  }
+});
+
+router.get("/:id/review", async (req, res) => {
+  if (req.session.user) {
+    try{
+      if (!req.params.id) throw "id needed";
+      if (!ObjectId.isValid(req.params.id)) throw "id is not a valid Object ID";
+      const menuItem = await menuData.get(req.params.id);
+      res.render("site/menuPages/menuItemReview", {menuItem: menuItem});
+    } catch(e) {
+      res.status(400).render("site/homepage", { error: e });
+    }
+  } else {
+    res.redirect("/");
+  }
+});
   
-  router.get("/logout", (req, res) => {
-    req.session.destroy();
-    res.render("site/logout", {});
-  });
-  
-  module.exports = router;
+module.exports = router;
